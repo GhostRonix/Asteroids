1d8dffafdb5a14ac6a897653e91038fdd2647ef8
GhostRonix <rlr_1995@hotmail.com>
2018-02-04 19:12:15 -0400

    Asignacion 1 atrasada!
    Atraso por Ausencia!

---
+++ src/BoilerPlate/MathUtilities.h
@@ -0,0 +1,42 @@
+#pragma once
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+namespace Math
+{
+
+
+	class MathUtilities
+	{
+	public:
+		/*====================
+		*	PUBLIC FUNCTIONS
+		======================*/
+		MathUtilities();
+		~MathUtilities();
+
+		inline int GetNearestInt(float);
+		inline float RoundFloatToInt(int);
+		template<class T> T MaximumN(T arr[4]);
+		template<class T> T MinimumN(T arr[4]);
+		template<class T> T Clamp(T);
+		template<class T> T Interpolate(T);
+		double ConvertAngle(double);
+		inline int IsPowerOfTwo(int);
+		double AngularDistances(double, double);
+
+	private:
+		/*======================
+		*		MEMBERS
+		======================*/
+		int intToFloat;
+		float floatToInt;
+		double radians;
+		double degrees;
+	};
+
+}
