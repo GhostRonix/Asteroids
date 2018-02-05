af5ed0cfc13724443944969d84998356aa924618
GhostRonix <rlr_1995@hotmail.com>
2018-02-04 23:29:50 -0400

    Cargando Modelos
    Creando Modelos de naves y Movimiento

---
+++ src/BoilerPlate/GameConfig.cpp
@@ -0,0 +1,247 @@
+#include "GameConfig.h"
+
+//
+#include <iostream>
+#include <algorithm>
+
+//
+#include "Configuration.h"
+#include "InputManager.h"
+//#include "Asteroid.hpp"
+
+namespace Asteroids
+{
+	Game::Game(int width, int height)
+		: m_scene(nullptr)
+		, m_width(width)
+		, m_height(height)
+		, m_player(nullptr)
+	{}
+
+	Game::~Game()
+	{
+		// Destroy the scene
+		//
+		delete m_scene;
+
+		// Clear list
+		//
+		//m_bullets.clear();
+	}
+
+	void Game::Init()
+	{
+		// Create the scene
+		//
+		m_scene = new Entities::Escenario(
+			Engine::Math::Vector3(0.1f, 0.1f, 0.15f),
+			m_width,
+			m_height
+		);
+
+		// Adding the player (ship)
+		//
+		CreatePlayer();
+
+		// Adding the enemies (asteroids)
+		//
+		//CreateAsteroids(10, Entities::Asteroid::AsteroidSize::BIG);
+	}
+
+	void Game::Update(double deltaTime)
+	{
+		// Handle Input
+		//
+		HandleInput();
+
+		// Update the scene
+		//
+		m_scene->Update(deltaTime);
+
+		// Look for collisions
+		//
+		//CheckCollisions();
+
+		// Clean up dead bullets
+		//
+		//CleanDeadUpBullets();
+	}
+
+	void Game::Render() const
+	{
+		// Render the game
+		//
+		m_scene->Render();
+	}
+
+	void Game::HandleInput()
+	{
+		if (Engine::Input::InputManager::Instance().IsKeyPressed('i'))
+		{
+			m_player->MoveUp();
+		}
+
+		if (Engine::Input::InputManager::Instance().IsKeyPressed('j'))
+		{
+			m_player->MoveLeft();
+		}
+
+		if (Engine::Input::InputManager::Instance().IsKeyPressed('l'))
+		{
+			m_player->MoveRight();
+		}
+
+		if (Engine::Input::InputManager::Instance().IsKeyReleased('k'))
+		{
+			m_player->ChangeShip();
+		}
+
+		/*if (Engine::Input::InputManager::Instance().IsKeyReleased(' '))
+		{
+			// If ship is respawning ignore shoot command
+			//
+			if (m_player->IsRespawning()) return;
+
+			// Disparar!
+			//
+			//Entities::Bullet* pBullet = m_player->Shoot();
+
+			// Store bullets in linked list
+			//
+			//m_bullets.push_back(pBullet);
+
+			// Add bullet to the scene
+			//
+			//m_scene->AddChild(pBullet);
+		}
+	}*/
+
+	void Game::CreatePlayer()
+	{
+		// Loading models
+		//
+		Utilities::Configuration config;
+		m_player = new Entities::Ship(config.LoadModels());
+
+		// Adding the player (ship)
+		//		
+		m_scene->AddChild(m_player);
+	}
+
+	/*void Game::CreateAsteroids(int amount, Entities::Asteroid::AsteroidSize::Size size, Engine::Math::Vector2 position) const
+	{
+		for (int i = 0; i < amount; ++i)
+		{
+			// Create new Asteroid
+			Entities::Asteroid* pAsteroid =
+				new Entities::Asteroid(size, position);
+
+			// Add asteroid to the scene
+			//
+			m_scene->AddChild(pAsteroid);
+
+			// If set on origin then move randomly around the 'world'
+			if (position == Engine::Math::Vector2::Origin)
+			{
+				pAsteroid->ApplyRandomTranslation();
+			}
+		}
+	}*/
+
+
+	/*---FRAGMENTACIOND DE ASTEROIDES--- rONIX*/
+
+	/*void Game::CreateDebris(Entities::Asteroid* currentAsteroid) const
+	{
+		// Retrieve current size
+		//
+		Entities::Asteroid::AsteroidSize::Size currentSize = currentAsteroid->GetSize();
+
+		// Create resulting asteroids from collision depending on current asteroid size
+		//
+		if (currentSize == Entities::Asteroid::AsteroidSize::BIG)
+		{
+			CreateAsteroids(2, Entities::Asteroid::AsteroidSize::MEDIUM, currentAsteroid->GetPosition());
+		}
+
+		if (currentSize == Entities::Asteroid::AsteroidSize::MEDIUM)
+		{
+			CreateAsteroids(2, Entities::Asteroid::AsteroidSize::SMALL, currentAsteroid->GetPosition());
+		}
+
+		// Remove from scene
+		//
+		m_scene->RemoveChild(currentAsteroid);
+	}*/
+
+	/*void Game::CleanDeadUpBullets()
+	{
+		if (m_bullets.size() == 0) return;
+
+		// Destroy one per update
+		//
+		auto iter = std::find_if(m_bullets.begin(), m_bullets.end(),
+			[&](Entities::Bullet* bullet) { return bullet->ShouldBeDeleted() || bullet->IsColliding(); });
+		if (iter != m_bullets.end())
+		{
+			DestroyBullet(*iter);
+		}
+	}*/
+
+	/*void Game::CheckCollisions()
+	{
+		if (!m_player->CanCollide()) return;
+
+		for (auto a : m_scene->GetChildren())
+		{
+			Entities::Asteroid* pAsteroid = dynamic_cast<Entities::Asteroid*>(a);
+			if (pAsteroid)
+			{
+				CheckCollisionWithPlayer(pAsteroid);
+				CheckCollisionWithBullets(pAsteroid);
+			}
+		}
+	}
+
+	void Game::CheckCollisionWithPlayer(Entities::Asteroid * pAsteroid) const
+	{
+		if (m_player->IntersectsWith(pAsteroid))
+		{
+			// Create debris
+			//
+			CreateDebris(pAsteroid);
+
+			//
+			m_player->Respawn();
+		}
+	}
+
+	void Game::CheckCollisionWithBullets(Entities::Asteroid * pAsteroid)
+	{
+		for (auto bullet : m_bullets)
+		{
+			if (bullet->IntersectsWith(pAsteroid))
+			{
+				// Create debris
+				//
+				CreateDebris(pAsteroid);
+			}
+		}
+	}
+
+	void Game::DestroyBullet(Entities::Bullet * bulletToDestroy)
+	{
+		// Remove bullet from the scene
+		//
+		m_scene->RemoveChild(bulletToDestroy);
+
+		// Remove bullet from list
+		//
+		auto bulletResult = find(m_bullets.begin(), m_bullets.end(), bulletToDestroy);
+		if (m_bullets.size() > 0 && bulletResult != m_bullets.end())
+		{
+			m_bullets.erase(bulletResult);
+		}
+	}
+}
+*/
---
+++ src/BoilerPlate/GameConfig.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#ifndef _GAME_H_
+#define _GAME_H_
+
+#include <list>
+
+#include "Escenario.h"
+#include "PlayerShip.h"
+/*
+#include "Bullet.hpp"
+#include "Asteroid.hpp"
+*/
+
+namespace Asteroids
+{
+	class Game
+	{
+	public:
+		/* =============================================================
+		* CTOR
+		* ============================================================= */
+		Game(int width, int height);
+		~Game();
+
+		/* =============================================================
+		* PUBLIC FUNCTIONS
+		* ============================================================= */
+		void Init();
+		void Update(double deltaTime);
+		void Render() const;
+	private:
+		/* =============================================================
+		* PRIVATE FUNCTIONS
+		* ============================================================= */
+		void HandleInput();
+		void CreatePlayer();
+		//void CreateAsteroids(int amount, Entities::Asteroid::AsteroidSize::Size size, Engine::Math::Vector2 position = Engine::Math::Vector2::Origin) const;
+		//void CreateDebris(Entities::Asteroid* currentAsteroid) const;
+		//void CleanDeadUpBullets();
+		//void CheckCollisions();
+		//void CheckCollisionWithPlayer(Entities::Asteroid* pAsteroid) const;
+		//void CheckCollisionWithBullets(Entities::Asteroid* pAsteroid);
+		//void DestroyBullet(Entities::Bullet* bulletToDestroy);
+
+		/* =============================================================
+		* MEMBERS
+		* ============================================================= */
+		Entities::Escenario* m_scene;
+		int m_width;
+		int m_height;
+		Entities::Ship* m_player;
+		//std::list<Entities::Bullet*> m_bullets;
+	};
+}
+
+#endif /* _GAME_H_ */
