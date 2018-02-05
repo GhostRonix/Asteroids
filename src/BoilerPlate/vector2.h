e4a376e9c9702b83ed1ff034293f722364ef6636
GhostRonix <rlr_1995@hotmail.com>
2018-02-04 20:08:12 -0400

    v2
    Vector2 header done!

--- src/BoilerPlate/vector2.h
+++ src/BoilerPlate/vector2.h
@@ -22,3 +22,38 @@ namespace Engine
 			Vector2();
 			Vector2(float, float);
 			Vector2(float);
+			/*============================
+			*	PUBLIC FUNCTIONS
+			============================*/
+			float Lenght() const;
+			float SquaredLenght() const;
+			Vector2 Normalize();
+
+			/*============================
+			*	OPERATORS
+			============================*/
+			Vector2& operator=(const Vector2& rhs);
+			Vector2& operator+=(const Vector2& rhs);
+			Vector2& operator-=(const Vector2& rhs);
+			Vector2& operator*=(const Vector2& rhs);
+			Vector2& operator/=(const Vector2& rhs);
+			Vector2 operator+(const Vector2& rhs) const;
+			Vector2 operator-(const Vector2& rhs) const;
+			Vector2 operator*(const Vector2& rhs) const;
+			Vector2 operator/(const Vector2& rhs) const;
+			bool	operator==(const Vector2& rhs) const;
+			bool	operator!=(const Vector2& rhs) const;
+			friend  Vector2 operator*(float, const Vector2&);
+			friend  Vector2 operator*(const Vector2&, float);
+
+			/*============================
+			*	MEMBERS
+			============================*/
+			float x;
+			float y;
+			float lenght;
+		};
+	}
+}
+
+#endif // !_VECTOR2_H_
