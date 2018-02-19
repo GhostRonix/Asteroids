594158b771ba01e9ec5fcf120dd6d2a99e0e3298
GhostRonix <rlr_1995@hotmail.com>
2018-02-19 18:33:01 -0400

    Agregando Clase Asteroids
    Clase Asteroids e includes de OPENGL

--- src/BoilerPlate/EnemyAsteroid.h
+++ src/BoilerPlate/EnemyAsteroid.h
@@ -1,19 +1,54 @@
 #pragma once
 
+#include <vector>
+
+#include "Entity.h"
+
+
+
 #ifndef _ENEMYASTEROID_H_
 #define _ENEMYASTEROID_H_
 
 namespace Asteroids
 {
 
-	class EnemyAsteroid: public Entity
-
-
+	class EnemyAsteroid 
 	{
 	public:
-		EnemyAsteroid();
-		~EnemyAsteroid();
-	};
-}
 
-#endif // !_ENEMYASTEROID_H_
+		struct AsteroidSize
+		{
+			enum Size
+			{
+				BIG = 0,
+				MEDIUM = 1,
+				SMALL = 2,
+
+
+			};
+		};
+	};
+
+	//Public Functuion Declarations
+
+	EnemyAsteroid   (AsteroidSize::Size, int, int);
+	void Impulse   (Engine::Math::Vector2) override;
+	void Update    (float);
+	void Render() override;
+	AsteroidSize::Size   GetSize();
+
+
+
+	//Private members
+private:
+
+	std::vector<Engine::Math::Vector2> m_points;
+	AsteroidSize::Size  m_size;
+
+	int m_sizeFactor;
+	float m_angle;
+	float m_rotation;
+
+};
+
+#endif // !_ENEMYASTEROID_H_               
---
+++ src/BoilerPlate/IncludeGL.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#ifndef _INCLUDE_GL_H_
+#define _INCLUDE_GL_H_
+
+//OpenGL includes
+
+#include <GL/glew.h>
+#include <SDL2/SDL_opengl.h>
+
+#endif // !_INCLUDE_GL_H_
